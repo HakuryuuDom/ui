41442405be055f958d274544f4e58d98ebc293cf
HakuryuuDom <abohn49@yahoo.com>
2020-08-03 16:44:54 -0400

    64 bit update (thanks caali and fruit)

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict'
 
 const Express = require('express'),
+	path = require('path'),
 	{host, port} = require('./config')
 
 class LazyServer {
-	constructor(dispatch) {
-		this.dispatch = dispatch
+	constructor(mod) {
+		this.mod = mod
 		this.q = null
 		this.app = null
 		this.port = 0
@@ -22,6 +23,8 @@ class LazyServer {
 					.set('env', 'production')
 					.enable('case sensitive routing')
 					.disable('x-powered-by')
+                    //init CoherentGT
+					.get('/_TERAToolbox/browserInit.js', (req, res) => { res.sendFile(path.join(__dirname, 'browserInit.js')); })
 					.use((req, res, next) => { this.router(req, res, next) })
 					.use((req, res) => { res.status(404).end() })
 					.use((err, req, res, next) => {
@@ -35,7 +38,7 @@ class LazyServer {
 				this.q = null
 
 				// Clean up on exit
-				this.dispatch.dispatch.connection.serverConnection.once('close', () => { this.app.close() })
+				this.mod.dispatch.connection.serverConnection.once('close', () => { this.app.close() })
 			}
 			else await this.q
 
@@ -46,21 +49,21 @@ class LazyServer {
 const servers = new WeakMap()
 
 async function getServer(router) {
-	const base = router.dispatch.dispatch
+	const base = router.mod.dispatch
 	if(servers.has(base)) return servers.get(base).get(router)
 
-	const server = new LazyServer(router.dispatch)
+	const server = new LazyServer(router.mod)
 	servers.set(base, server)
 	return server.get(router)
 }
 
-function UI(dispatch, options) { return UI.Router(dispatch, options) }
+function UI(mod, options) { return UI.Router(mod, options) }
 
 Object.assign(UI, Express, {
-	Router(dispatch, options) {
+	Router(mod, options) {
 		const router = Express.Router(options)
 		Object.setPrototypeOf(router, UI.Router.prototype)
-		router.dispatch = dispatch
+		router.mod = mod
 		return router
 	}
 })
@@ -68,8 +71,9 @@ Object.assign(UI, Express, {
 UI.Router.prototype = Object.assign({}, Express.Router, {
 	async open(path = '/') {
 		if(!path.startsWith('/')) path = '/' + path
-
-		this.dispatch.toClient('S_OPEN_AWESOMIUM_WEB_URL', 1, {url: await getServer(this) + path})
+		let url = await getServer(this) + path
+		url = url.replace("http//", "http://") //is this needed? shits weird man...
+		this.mod.send('S_OPEN_AWESOMIUM_WEB_URL', 1, {url})
 	}
 })
 
